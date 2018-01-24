bcf6363ec1880701c8f1f8f6408aa50e376604c3
cliveportman <clive@cliveportman.co.uk>
2018-01-24 11:00:49 +0000

    Fix for additional cart being created

--- services/CurrencySwitcherService.php
+++ services/CurrencySwitcherService.php
@@ -4,11 +4,11 @@ namespace Craft;
 class CurrencySwitcherService extends BaseApplicationComponent
 {
 
-	public function switchCurrency($isoCurrency) 
+	public function switchCurrency($isoCurrency, $cart) 
 	{
 
         // CHECK FOR A CART AND IF THERE ISN'T ONE, CREATE ONE
-        $cart = craft()->commerce_cart->getCart();   
+        //$cart = craft()->commerce_cart->getCart();   
         if (!$cart->id) {
             if (!craft()->commerce_orders->saveOrder($cart)) {
                 $error = Craft::t('Error creating empty cart: ') . print_r($order->getAllErrors(), true);
@@ -26,9 +26,9 @@ class CurrencySwitcherService extends BaseApplicationComponent
 
         // SET THE CART'S PAYMENT CURRENCY
         if (!craft()->commerce_cart->setPaymentCurrency($cart, $currency)) {
-            $error = Craft::t('Error on setting currency: ') . print_r($cart->getAllErrors(), true);
-            CurrencySwitcherPlugin::log($error, LogLevel::Error);
-            throw new Exception($error);
+            //$error = Craft::t('Error on setting currency: ') . print_r($cart->getAllErrors(), true);
+            //CurrencySwitcherPlugin::log($error, LogLevel::Error);
+            //throw new Exception($error);
         }
 
         return $currency;
