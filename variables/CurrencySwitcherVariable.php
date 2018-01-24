91ee219b36d72ba359646a411cee9f7ea8666e93
cliveportman <clive@cliveportman.co.uk>
2018-01-24 11:00:32 +0000

    Fix for PHP error with naming of function

--- variables/CurrencySwitcherVariable.php
+++ variables/CurrencySwitcherVariable.php
@@ -5,10 +5,10 @@ namespace Craft;
 class CurrencySwitcherVariable
 {
 
-    public function switch($isoCurrency)
+    public function switchCurrency($isoCurrency, $cart)
     {
         
-        $currency = craft()->currencySwitcher->switchCurrency($isoCurrency);
+        $currency = craft()->currencySwitcher->switchCurrency($isoCurrency, $cart);
         return $currency;
 
     }
